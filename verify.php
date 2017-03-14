4fde06527e8337ea21f4461e1b71b55bebb10857
sikhapun <sikhapun34@gmail.com>
2017-03-14 22:51:32 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,15 @@
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
-...
+<?php
+$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
